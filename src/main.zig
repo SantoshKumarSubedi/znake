598585c11085c1e6b63ee66b1366b06a87dbb2e9
santosh kumar subedi <subedisantos9@gmail.com>
2024-08-21 08:49:51 +0545

    moving

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@
 const std = @import("std");
 const r1 = @import("raylib");
 const print = @import("std").debug.print;
-const GameState = struct { allocator: std.mem.Allocator, time: f32 = 0, radius: f32 = 0, movement: KeyPressed, snake: SnakeLocation, length: i32 };
+const GameState = struct { allocator: std.mem.Allocator, time: f32 = 0, radius: f32 = 0, movement: KeyPressed, snake: *SnakeLocation, length: i32 };
 const KeyPressed = struct {
     left: bool,
     right: bool,
@@ -13,28 +13,45 @@ const SnakeLocation = struct {
     x: i32,
     y: i32,
     next: ?*SnakeLocation,
-    pub fn create(allocator: *std.mem.Allocator,x:i32,y:i32) !*SnakeLocation{
-        const location =  allocator.create(SnakeLocation) catch @panic("Out of memory.");
-        location.* = SnakeLocation{.x=x,.y=y,.next=null};
+    pub fn create(allocator: *std.mem.Allocator, x: i32, y: i32) !*SnakeLocation {
+        const location = allocator.create(SnakeLocation) catch @panic("Out of memory.");
+        location.* = SnakeLocation{ .x = x, .y = y, .next = null };
         return location;
     }
 
-    pub fn append(self:*SnakeLocation,allocator:*std.mem.Allocator,x:i32,y:i32) !void{
+    pub fn append(self: *SnakeLocation, allocator: *std.mem.Allocator, x: i32, y: i32) !void {
         var current = self;
-        while(current.next) |n| {
-            current=n;
+        while (current.next) |n| {
+            current = n;
         }
-         const location =  allocator.create(SnakeLocation) catch @panic("Out of memory.");
-        location.* = SnakeLocation{.x=x,.y=y,.next=null};
-        current.next=location;
+        const location = allocator.create(SnakeLocation) catch @panic("Out of memory.");
+        location.* = SnakeLocation{ .x = x, .y = y, .next = null };
+        current.next = location;
     }
 
-    pub fn deint(self:*SnakeLocation) void {
-        if(self.next) |n| {
+    pub fn deint(self: *SnakeLocation) void {
+        if (self.next) |n| {
             n.deint();
         }
         const allocator = std.heap.page_allocator;
         allocator.destroy(self);
+    }
+
+    pub fn getTail(head: *SnakeLocation) *SnakeLocation {
+        var current = head;
+        var prev = current;
+        print("................................\n", .{});
+        while (current.next) |n| {
+            // print("a", .{});
+            current = n;
+            prev = n;
+            print("{?}\n", .{prev});
+            std.time.sleep(1000000000);
+        }
+        prev.next = null;
+        print("{?}\n", .{prev});
+        print("{?}\n", .{current});
+        return current;
     }
 };
 const SnakeLocationVector = []@Vector(2, i32);
@@ -50,9 +67,9 @@ pub fn main() anyerror!void {
 
     r1.setTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-
     // Main game loop
     while (!r1.windowShouldClose()) { // Detect window close button or ESC key
+        // std.time.sleep(0.5 * 1000000000);
         // Update
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
@@ -62,13 +79,9 @@ pub fn main() anyerror!void {
         //----------------------------------------------------------------------------------
         r1.beginDrawing();
         defer r1.endDrawing();
-        keyPressed(game_state);
+
         gameTick(game_state);
         gameDraw(game_state);
-        // c.clearBackground(c.Color.white);
-
-        // r1.drawText("Congrats! You created your first window!", 190, 200, 20, r1.Color.light_gray);
-        //----------------------------------------------------------------------------------
     }
 }
 
@@ -76,16 +89,14 @@ fn gameInit() *GameState {
     var allocator = std.heap.page_allocator;
     const game_state = allocator.create(GameState) catch @panic("Out of memory.");
     const key_pressed = KeyPressed{ .left = false, .right = false, .up = false, .down = false };
-    // defer vectors.deinit();
-    
-   
-    
-    
-    const location = try SnakeLocation.create(&allocator, 10, 10);
-    // print("{}",location);
-    location.append(&location,&allocator, 10, 10);
-    
-    game_state.* = GameState{ .allocator = allocator, .radius = 10.0, .movement = key_pressed, .snake = location.*, .length = 10};
+
+    const location = try SnakeLocation.create(&allocator, 150, 10);
+    for (1..10) |i| {
+        const a: i32 = @intCast(i);
+        try location.append(&allocator, @intCast(location.x - (11 * a)), 10);
+    }
+
+    game_state.* = GameState{ .allocator = allocator, .radius = 10.0, .movement = key_pressed, .snake = location, .length = 10 };
     return game_state;
 }
 
@@ -95,43 +106,39 @@ fn gameTick(game_state: *GameState) void {
 
 fn gameDraw(game_state: *GameState) void {
     r1.clearBackground(r1.Color.white);
-
-    // Create zero terminated string with the time and radius.
-    // var buf: [256]u8 = undefined;
-    // const slice = std.fmt.bufPrintZ(
-    //     &buf,
-    //     "time: {d:.02}, radius: {d:.02}",
-    //     .{ game_state.time, game_state.radius },
-    // ) catch "error";
-    // // print("{s}", .{slice});
-    // r1.drawText(slice, 10, 10, 20, r1.Color.black);
-
-    // Draw a circle moving across the screen with the config radius.
-    // if (game_state.movement.left and game_state.snake.x > 1) {
-    //     game_state.snake.x -= 1;
-    // }
-    // if (game_state.movement.right and game_state.snake.x < screen_w) {
-    //     game_state.snake.x += 1;
-    // }
-    // if (game_state.movement.up and game_state.snake.y > 0) {
-    //     game_state.snake.y -= 1;
-    // }
-    // if (game_state.movement.down and game_state.snake.y < screen_h) {
-    //     game_state.snake.y += 1;
-    // }
     var current = game_state.snake;
-    print("{}", .{@TypeOf(current)});
- while(@TypeOf(current)==SnakeLocation){
-        
+    var prev: *SnakeLocation = game_state.snake;
+    while (current.next != null) {
         r1.drawRectangle(current.x, current.y, 10, 10, r1.Color.blue);
-        if(current.next==null){
+        if (current.next == null) {
             break;
-        }else{
-        current=current.next.?.*;
+        } else {
+            prev = current;
+            current = current.next.?;
+        }
     }
+    keyPressed(game_state);
+    const head = game_state.snake;
+    if (game_state.movement.right) {
+        current.x = head.x + 11;
+        current.y = head.y;
     }
-    // const circle_x: f32 = @mod(game_state.time * 50.0, screen_w);
-    
+    if (game_state.movement.left) {
+        current.x = head.x - 11;
+        current.y = head.y;
+    }
+    if (game_state.movement.up) {
+        current.y = head.y - 11;
+        current.x = head.x;
+    }
+
+    if (game_state.movement.down) {
+        current.y = head.y + 11;
+        current.x = head.x;
+    }
+    current.next = head;
+    prev.next = null;
+    game_state.snake = current;
 }
 
 fn keyPressed(game_state: *GameState) void {
